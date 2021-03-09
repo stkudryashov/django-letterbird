e9b94225660aac55d33e335d3c5c1bfffbcb3644
stkudryashov <aleks59niki30@gmail.com>
2021-03-09 20:15:43 +0300

    fix button and login redirect

--- letterbird/users/views.py
+++ letterbird/users/views.py
@@ -6,30 +6,36 @@ from .forms import UserRegisterForm, UserLoginForm
 
 
 def user_register(request):
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Регистрация прошла успешно')
-            return redirect('login')
-        else:
-            messages.error(request, 'Ошибка регистрации')
+    if request.user.is_authenticated:
+        return redirect('homepage')
     else:
-        form = UserRegisterForm()
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Регистрация прошла успешно')
+                return redirect('login')
+            else:
+                messages.error(request, 'Ошибка регистрации')
+        else:
+            form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
 def user_login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('homepage')
-        else:
-            messages.error(request, 'Ошибка входа')
+    if request.user.is_authenticated:
+        return redirect('homepage')
     else:
-        form = UserLoginForm()
+        if request.method == 'POST':
+            form = UserLoginForm(data=request.POST)
+            if form.is_valid():
+                user = form.get_user()
+                login(request, user)
+                return redirect('homepage')
+            else:
+                messages.error(request, 'Ошибка входа')
+        else:
+            form = UserLoginForm()
     return render(request, 'users/login.html', {'form': form})
 
 
