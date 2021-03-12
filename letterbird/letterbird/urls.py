2101da1ed74a70fc655b9727824bf5a6faa28fc8
stkudryashov <aleks59niki30@gmail.com>
2021-03-12 19:22:57 +0300

    update admin panel

--- letterbird/letterbird/urls.py
+++ letterbird/letterbird/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('grappelli/', include('grappelli.urls')),
     path('admin/', admin.site.urls),
     path('', include('letters.urls')),
     path('user/', include('users.urls')),
